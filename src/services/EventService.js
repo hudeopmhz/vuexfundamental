316592fbd3432c419e557224e3ee6ae5e214e3d9
Prajwol <hudeopmhz@gmail.com>
2021-08-31 09:08:16 +1000

    MyJsonServer link added for data

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://my-json-server.typicode.com/hudeopmhz/database",
   withCredentials: false,
   headers: {
     Accept: "application/json",
